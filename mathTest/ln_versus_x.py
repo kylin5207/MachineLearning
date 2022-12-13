09e255e7104930ab14adede9de019f05fe528517
Kylin <38106679+kylin5207@users.noreply.github.com>
2022-12-13 09:41:08 +0800

    feat: x > ln(x+1), when x > 0

---
+++ mathTest/ln_versus_x.py
@@ -0,0 +1,18 @@
+"""
+ln(x+1)与x的关系:
+x > ln(x+1)
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 10, 100)
+y = np.log(x+1)
+
+plt.plot(x, x, color="g", label=r"$y=x$")
+plt.plot(x, y, color="b", label=r"$y=ln(x)$")
+plt.legend()
+plt.xlim(0, 11)
+plt.xlabel(r"$x$")
+plt.ylabel(r"$y$")
+plt.legend()
+plt.show()
