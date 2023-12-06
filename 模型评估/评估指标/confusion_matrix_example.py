3ee5533e61461eca8187269bfd31bbfb8f74bc54
Kylin <38106679+kylin5207@users.noreply.github.com>
2023-12-06 13:49:40 +0800

    feat: confusion matrix example

---
+++ 模型评估/评估指标/confusion_matrix_example.py
@@ -0,0 +1,43 @@
+"""
+confusion_matrix 参数说明
+- y_true:
+  类型：array-like（数组形式的对象），形状 [n_samples]。
+  描述：这是真实的目标值。它包含了每个样本的实际分类标签。
+
+- y_pred:
+  类型：array-like，形状 [n_samples]。
+  描述：这是模型预测的目标值。它包含了每个样本的预测分类标签。
+
+- labels （可选）:
+  类型：array-like，形状 [n_classes]。
+  描述：这是用于构建混淆矩阵的标签数组。如果未指定，则从 y_true 和 y_pred 中自动推断出标签。
+
+- sample_weight （可选）:
+  类型：array-like，形状 [n_samples]。
+  描述：样本权重。如果提供，则每个样本的贡献将根据其权重进行缩放，这对于处理非均衡数据集或赋予某些样本更高的重要性非常有用。默认为 None。
+
+"""
+import numpy as np
+from sklearn.metrics import confusion_matrix
+
+def compute_confusion_matrix(labels, pred_labels_list, true_labels_list):
+    # y_pred
+    y_pred = np.asarray(pred_labels_list)
+    y_true = np.asarray(true_labels_list)
+    matrix = confusion_matrix(y_true,
+                              y_pred,
+                              labels=labels)
+    return matrix
+
+
+# 真实标签
+y_true = np.asarray([2, 0, 2, 2, 0, 1])
+labels = np.unique(y_true)
+
+# 预测标签
+y_pred = np.asarray([0, 0, 2, 2, 0, 2])
+
+# 计算混淆矩阵
+cm = compute_confusion_matrix(labels,y_pred, y_true)
+
+print(cm)
